73af3404f04d460b37844f17a8ce95464ce1a4e5
구환준/모건 <koojun99@naver.com>
2023-07-13 20:05:05 +0900

    나이별 필터링 시 데이터 활용방법 변경

--- KnockKnock/src/main/java/com/example/knockknock/domain/repository/PostRepository.java
+++ KnockKnock/src/main/java/com/example/knockknock/domain/repository/PostRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByBoard(Board board);
+
+    List<Post> findByBoardAndMemberAgeBetween(Board board, int ageGroupStart, int ageGroupEnd);
 }
