386907b198adbfef27bbb8ac260a79193a346a15
SoMin <thals0107@naver.com>
2023-07-16 18:13:40 +0900

    #3 feat: Create GlobalException init settings

---
+++ KnockKnock/src/main/java/com/example/knockknock/global/exception/GlobalException.java
@@ -0,0 +1,10 @@
+package com.example.knockknock.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GlobalException extends RuntimeException {
+    private final GlobalErrorCode errorCode;
+}
