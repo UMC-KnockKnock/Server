317d659a067f27c10f25ea6d4758c6ff3a96a660
SoMin <thals0107@naver.com>
2023-07-25 17:06:19 +0900

    feat: detailsimpl init

---
+++ src/main/java/com/example/knockknock/domain/member/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.knockknock.domain.member.security;
+
+import com.example.knockknock.domain.member.entity.Member;
+import com.example.knockknock.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+// 로그인시 유저 정보를 가저오는 역할 클래스
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        Member member = memberRepository.findById(Long.valueOf(id))
+            .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        return new UserDetailsImpl(member, id);
+    }
+
+//    @Override
+//    public UserDetails loadUserById(Long id) throws UsernameNotFoundException {
+//        User user = userRepository.findById(id) .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+//        return new UserDetailsImpl(user, user.getEmail());
+//    }
+}
