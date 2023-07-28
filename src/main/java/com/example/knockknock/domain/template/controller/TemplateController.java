d475394ab91b425d0eb63b74ff1f86c309702afc
SoMin <thals0107@naver.com>
2023-07-28 13:29:35 +0900

    #35 feat: Create template service

--- src/main/java/com/example/knockknock/domain/template/controller/TemplateController.java
+++ src/main/java/com/example/knockknock/domain/template/controller/TemplateController.java
@@ -1,14 +1,28 @@
 package com.example.knockknock.domain.template.controller;
 
+import com.example.knockknock.domain.template.repository.TemplateRepository;
+import com.example.knockknock.domain.template.service.TemplateService;
+import com.example.knockknock.global.message.ResponseMessage;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Tag(name = "Template", description = "Template API")
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/template")
 public class TemplateController {
-    //- [ ]  멘트 템플릿으로 이동 /friends/{friendId}/template
+    // 멘트 템플릿으로 이동 /template
+    private final TemplateService templateService;
+    @Operation(summary = "템플릿 보기", description = "템플릿 보기")
+    @GetMapping()
+    public ResponseEntity getTemplate(){
+        return ResponseMessage.SuccessResponse("템플릿 조회 성공", templateService.getTemplate());
+    }
 }
--- src/main/java/com/example/knockknock/domain/template/service/TemplateService.java
+++ src/main/java/com/example/knockknock/domain/template/service/TemplateService.java
@@ -1,11 +1,26 @@
 package com.example.knockknock.domain.template.service;
 
+import com.example.knockknock.domain.template.dto.responseDto.TemplateResponseDto;
+import com.example.knockknock.domain.template.entity.Template;
 import com.example.knockknock.domain.template.repository.TemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TemplateService {
     private final TemplateRepository templateRepository;
+
+    public List<TemplateResponseDto> getTemplate() {
+        List<Template> templates = templateRepository.findAll();
+        List<TemplateResponseDto> templateResponseDtos = new ArrayList<>();
+        for (Template template : templates) {
+            TemplateResponseDto templateResponseDto = new TemplateResponseDto(template);
+            templateResponseDtos.add(templateResponseDto);
+        }
+        return templateResponseDtos;
+    }
 }
