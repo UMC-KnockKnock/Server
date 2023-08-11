03ab65177b3c6b6a1dc331473a81259c7cdbf576
SoMin <thals0107@naver.com>
2023-08-11 14:04:40 +0900

    feat: templatetag enum 추가

--- src/main/java/com/example/knockknock/domain/template/entity/TemplateTag.java
+++ src/main/java/com/example/knockknock/domain/template/entity/TemplateTag.java
@@ -1,8 +1,8 @@
 package com.example.knockknock.domain.template.entity;
 
 public enum TemplateTag {
-    // 설날, 추석, 생일, 결혼식, 장례식, 휴가, 일반축하, 위로, 사과, 안부인사, 승진축하
-    // Lunar New Year, Chuseok, Birthday, Wedding, Funeral, Vacation, General Celebration, Consolation, Apology, Greetings, Promotion Celebration
+    // 설날, 추석, 생일, 결혼식, 장례식, 휴가, 일반축하, 약속, 위로, 사과, 안부인사, 승진축하
+    // Lunar New Year, Chuseok, Birthday, Wedding, Funeral, Vacation, General Celebration, Appointment, Consolation, Apology, Greetings, Promotion Celebration
 
     LUNAR_NEW_YEAR,
     CHUSEOK,
@@ -11,6 +11,7 @@ public enum TemplateTag {
     FUNERAL,
     VACATION,
     GENERAL_CELEBRATION,
+    APPOINTMENT,
     CONSOLATION,
     APOLOGY,
     GREETINGS,
