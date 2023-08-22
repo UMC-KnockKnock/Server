b7d6a61b5f8954a8313b556c541145a71ae2c640
구환준/모건 <koojun99@naver.com>
2023-08-22 20:34:41 +0900

    Gathering location recommendation modified

---
+++ src/main/java/com/example/knockknock/domain/gathering/dto/request/LocationRecommendRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.knockknock.domain.gathering.dto.request;
+
+import lombok.*;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationRecommendRequestDto {
+    private String location;
+}
