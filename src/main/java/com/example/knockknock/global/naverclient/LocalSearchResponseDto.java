ee51c48ba8f3faadd953321ca6364b3915d010f8
구환준/모건 <koojun99@naver.com>
2023-08-22 21:12:06 +0900

    장소 추천에 사용자가 입력한 장소 좌표 추가

--- src/main/java/com/example/knockknock/global/naverclient/LocalSearchResponseDto.java
+++ src/main/java/com/example/knockknock/global/naverclient/LocalSearchResponseDto.java
@@ -12,6 +12,6 @@ import java.util.List;
 public class LocalSearchResponseDto {
     private Double locationMapx;  // 사용자가 입력한 location의 x 좌표
     private Double locationMapy;  // 사용자가 입력한 location의 y 좌표
-    private List<LocalSearchItem> recommendations; // XML 포멧에서는 item 태그로, JSON 포멧에서는 items 속성으로 표현된다. 개별 검색 결과이며 title, link, description, address, mapx, mapy를 포함한다.
+    private List<LocalSearchItem> items; // XML 포멧에서는 item 태그로, JSON 포멧에서는 items 속성으로 표현된다. 개별 검색 결과이며 title, link, description, address, mapx, mapy를 포함한다.
 
 }
